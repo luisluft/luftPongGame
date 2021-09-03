905d47563e3a9dc9fc07334a49a1435a859aaf73
luisluft <luberluft@gmail.com>
2021-09-03 10:32:27 -0400

    add animation to the game

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const canvas = document.createElement("canvas");
 const context = canvas.getContext("2d");
 const width = 500;
 const height = 700;
-const screenWidth = window.screen.width;
+const screenWidth = body.clientWidth;
 const canvasPosition = screenWidth / 2 - width / 2;
 const isMobile = window.matchMedia("(max-width: 600px)");
 const gameOverEl = document.createElement("div");
@@ -205,6 +205,7 @@ function animate() {
   ballMove();
   ballBoundaries();
   computerAI();
+  window.requestAnimationFrame(animate);
 }
 
 // Start Game, Reset Everything
@@ -220,8 +221,9 @@ function startGame() {
   createCanvas();
   animate();
   canvas.addEventListener("mousemove", (e) => {
-    console.log(e.clientX);
+    // console.log(e.clientX);
     playerMoved = true;
+
     // Compensate for canvas being centered
     paddleBottomX = e.clientX - canvasPosition - paddleDiff;
     if (paddleBottomX < paddleDiff) {
@@ -236,4 +238,4 @@ function startGame() {
 }
 
 // On Load
-// startGame();
+startGame();
